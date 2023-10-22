cb99628ac6a480b2b3c691a25471098156001d63
yuri_k <krenev_yu@mail.ru>
2023-10-22 23:40:29 +0400

    rename 1->01, add 02

---
+++ 01.py
@@ -0,0 +1,8 @@
+print("hello neo")
+print("hello 'neo' ")
+
+var_1 = -5
+var_2 = 999
+result = var_1 + var_2
+print(result)
+print(type(var_1))
---
+++ 02.py
@@ -0,0 +1,51 @@
+num_1 = 5
+print(num_1)
+print(type(num_1))
+
+# Сложение
+num_2 = 3
+num_3 = 8
+result = num_2 + num_3
+print(result)
+result = -(num_2 + num_3)
+print(result)
+
+# Вычитание
+num_4 = 80
+num_5 = 16
+result = num_4 - num_5
+print(result)
+
+# Умножение
+num_6 = 35
+num_7 = 5
+result = num_6 * num_7
+print(result)
+
+# Возведение в степень
+num_8 = 3
+num_9 = 3
+result = num_8 ** num_9
+print(result)
+
+# Деление
+num_10 = 15
+num_11 = 6
+result = num_10 / num_11
+print(result)
+
+# остаток от деления
+num_12 = 80
+num_13 = 6
+result = num_12 % num_13
+print(result)
+
+# сравнение
+num_14 = 80
+num_15 = 6
+print(num_14 == num_15)
+print(num_14 > num_15)
+print(num_14 < num_15)
+print(num_14 >= num_15)
+print(num_14 <= num_15)
+print(num_14 != num_14)
