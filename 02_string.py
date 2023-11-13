d800d45498ddafc0c9d032763969e886ef5d2ceb
yuri_k <krenev_yu@mail.ru>
2023-11-13 19:39:43 +0400

    rename 1->01

---
+++ 02_string.py
@@ -0,0 +1,16 @@
+# str_1 = "hello"
+# str_2 = "world"
+# result = str_1 + str_2
+# print(str_1)
+# print(str_2)
+# print(result)
+
+# str_3 = "hello "
+# str_4 = "world "
+# result_2 = str_3 * 2 + str_4 * 2
+# print(result_2 * 3)
+
+var_1 = 5
+var_2 = 10
+result_3 = var_1 + var_2
+print("Результат: " + str(result_3))
