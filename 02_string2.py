4219474c105dd42a40c4518174bfb639a7c9eb49
yuri_k <krenev_yu@mail.ru>
2023-12-30 16:58:06 +0400

    rename 1->01

---
+++ 02_string2.py
@@ -0,0 +1,13 @@
+str_1 = 'hello'
+str_2 = 'WORLD'
+print(str_1)
+print(dir(str_1))
+print(str_1.upper())
+print(str_1.title())
+print(str_2.lower())
+
+first_name = "Ivan"
+last_name = 'Ivanov'
+a = '{} {}'
+result = a.format(first_name, last_name)
+print('My name is: ' + result)
