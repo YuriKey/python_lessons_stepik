609a42838a6d952861a620912fba1bc128221b6c
yuri_k <krenev_yu@mail.ru>
2024-03-09 09:57:31 +0400

    rename 1->01

--- 13_dict.py
+++ 13_dict.py
@@ -1,17 +1,17 @@
 # # список
 #
-# family_1 = ['Frank', 'Fiona', 'Lip', 'Yan', 'Deby', 'Karl', 'Fiona']
-# print(family_1)
-# print(len(family_1))
+family_1 = ['Frank', 'Fiona', 'Lip', 'Yan', 'Deby', 'Karl', 'Fiona']
+print(family_1)
+print(len(family_1))
 #
 # # множества
-# family_2 = {'Frank', 'Fiona', 'Lip', 'Yan', 'Deby', 'Karl', 'Fiona'}
-# print(family_2)
-# print(len(family_2))
+family_2 = {'Frank', 'Fiona', 'Lip', 'Yan', 'Deby', 'Karl', 'Fiona'}
+print(family_2)
+print(len(family_2))
 
 # словарь (ключ:значение)
 family_3 = {'dad': 'Frank', 'daughter': 'Fiona', 'son': 'Lip', 'son2': 'Yan'}
-# print(family_3['daughter'])
-# print(len(family_3))
+print(family_3['daughter'])
+print(len(family_3))
 for k, v in family_3.items():
     print(k)
---
+++ 14_files.py
@@ -0,0 +1,21 @@
+# fw = open('files/file.txt', 'a')
+# fw.write('hello neo\n')
+# fw.close()
+
+# var = input("what is your name? ")
+# fw = open('files/file.txt', 'a')
+# fw.write(var)
+# fw.close()
+#
+# var = input("what is your name? ")
+# fw = open('files/file1.txt', 'w')
+# fw.write(var)
+# fw.close()
+
+
+fw = open('files/file.txt', 'r+')
+var = fw.read()
+fw.write('hello neo\n')
+fw.write('but i`m chuck\n')
+fw.close()
+print(var)
